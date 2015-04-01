5af4be5b6b8bdd17137369ff912c56a903f1d903
Atul Acharya <laventura@users.noreply.github.com>
2015-04-01 00:24:38 -0700

    reverb, echo functionality

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -30,7 +30,6 @@ class PlaySoundsViewController: UIViewController {
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
 
     }
-
     
     // stop all audio and reset
     func stopAllAudio() {
@@ -45,65 +44,6 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.rate = rate
         audioPlayer.play()
     }
-    
-    
-    
-    @IBAction func playSlowAudio(sender: UIButton) {
-        
-        refreshAndPlay(0.5) // play 0.5x the normal speed
-    }
-
-    @IBAction func playFastAudio(sender: UIButton) {
-        refreshAndPlay(2.0)  // play 2x the normal speed
-    }
-    
-    @IBAction func stopAudio(sender: AnyObject) {
-        stopAllAudio()
-    }
-    
-    @IBAction func playChipmunkEffect(sender: AnyObject) {
-        // ORIGINAL
-        // playAudioWithVariablePitch(1000)
-        // create some effect (higher pitch) with UnitTimePitch
-        var changePitchEffect = AVAudioUnitTimePitch()
-        changePitchEffect.pitch = 1000     // chipmunk: high pitch effect
-        
-        playEffect(changePitchEffect)
-
-     }
-    
-    @IBAction func playDarthEffect(sender: AnyObject) {
-        //// playAudioWithVariablePitch(-1000)
-        
-        // create some effect (higher pitch) with UnitTimePitch
-        var changePitchEffect = AVAudioUnitTimePitch()
-        changePitchEffect.pitch = -1000     // DARTH effect: low pitch
-        
-        playEffect(changePitchEffect)
-
-        
-    }
-    
-    // generate a reasonable echo without overwhelming the user
-    @IBAction func playEchoEffect(sender: UIButton) {
-        var echoEffectNode = AVAudioUnitDelay()
-        echoEffectNode.delayTime = 0.08     // 80 ms
-        echoEffectNode.feedback = 40        // 40%
-        
-        playEffect(echoEffectNode)
-    }
-    
-    // reverb effect
-    @IBAction func playReverbEffect(sender: UIButton) {
-        // create Reverb effect
-        var reverb = AVAudioUnitReverb()
-        reverb.wetDryMix = Float(80.0)      // stronger mix of effect
-        reverb.loadFactoryPreset(AVAudioUnitReverbPreset.LargeChamber)
-        
-        playEffect(reverb)
-        
-    }
-    
     
     // helper func to play effect based on given effectNode
     func playEffect(effectNode: AVAudioNode) {
@@ -124,10 +64,55 @@ class PlaySoundsViewController: UIViewController {
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         audioEngine.startAndReturnError(nil)
         audioPlayerNode.play()
-        
-        
     }
 
+    // Actions
     
+    @IBAction func playSlowAudio(sender: UIButton) {
+        refreshAndPlay(0.5) // play 0.5x the normal speed
+    }
 
+    @IBAction func playFastAudio(sender: UIButton) {
+        refreshAndPlay(2.0)  // play 2x the normal speed
+    }
+    
+    @IBAction func stopAudio(sender: AnyObject) {
+        stopAllAudio()
+    }
+    
+    @IBAction func playChipmunkEffect(sender: AnyObject) {
+        // create some effect (higher pitch) with UnitTimePitch
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = 1000     // chipmunk: high pitch effect
+        
+        playEffect(changePitchEffect)
+     }
+    
+    @IBAction func playDarthEffect(sender: AnyObject) {        
+        // create some effect (higher pitch) with UnitTimePitch
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = -1000     // DARTH effect: low pitch
+        
+        playEffect(changePitchEffect)
+    }
+    
+    // generate a reasonable echo without overwhelming the user
+    @IBAction func playEchoEffect(sender: UIButton) {
+        var echoEffectNode = AVAudioUnitDelay()
+        echoEffectNode.delayTime = 0.08     // 80 ms
+        echoEffectNode.feedback = 40        // 40%
+        
+        playEffect(echoEffectNode)
+    }
+    
+    // reverb effect
+    @IBAction func playReverbEffect(sender: UIButton) {
+        // create Reverb effect
+        var reverb = AVAudioUnitReverb()
+        reverb.wetDryMix = Float(80.0)      // stronger mix of effect
+        reverb.loadFactoryPreset(AVAudioUnitReverbPreset.LargeChamber)
+        
+        playEffect(reverb)
+    }
+    
 }
